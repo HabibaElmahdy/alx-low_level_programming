5e6fc02fef906bfc190d75410f2349edd8ec94b4
HabibaElmahdy <habibaelmahdy26@gmail.com>
2023-04-11 03:55:31 -0700

    11/4

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Print _putchar
+ * Description: prints _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+char str[] = "_putchar";
+int i = 0;
+
+while (str[i] != 0)
+{
+char c = str[i];
+
+putchar(c);
+i++;
+}
+putchar(n);
+return (0);
+}
